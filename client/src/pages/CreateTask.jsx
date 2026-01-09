0d325a872bec1970ecd49c2d0b42d5e31e010d37
Abhinand <abhinandatr@gmail.com>
2026-01-10 00:32:25 +0530

    styled create task

--- client/src/pages/CreateTask.jsx
+++ client/src/pages/CreateTask.jsx
@@ -43,55 +43,90 @@ const CreateTask = () =>{
     }
     }
 
-    return(
-        <>
-            <Navbar/>
-        <div>
-            <h2>Create Task</h2>
+    return (
+      <>
+        <Navbar />
+        <div className="min-h-screen flex items-center justify-center bg-slate-900">
+          <div className="w-full max-w-md bg-white/10 rounded-lg px-3 py-4">
+            <h2 className="text-2xl font-semibold text-center text-slate-100 mb-6">
+              Create Task
+            </h2>
 
-            <div>
-                <button onClick={() => setType("personal")}>My Task</button>
-                <button onClick={() => setType("assign")}>Assign Task</button>
+            <div className="flex gap-3 mb-6">
+              <button
+                onClick={() => setType("personal")}
+                className={`flex-1 py-2 rounded-lg text-medium text-sm transition 
+                    ${
+                      type === "personal"
+                        ? "bg-blue-500 text-white"
+                        : "bg-slate-100 text-slate-600 hover:bg-slate-200"
+                    }`}
+              >
+                My Task
+              </button>
+              <button
+                onClick={() => setType("assign")}
+                className={`flex-1 py-2 rounded-lg text-medium text-sm transition 
+                    ${
+                      type === "assign"
+                        ? "bg-blue-500 text-white"
+                        : "bg-slate-100 text-slate-600 hover:bg-slate-200"
+                    }`}
+              >
+                Assign Task
+              </button>
             </div>
 
             <form onSubmit={handleSubmit}>
-                <input name="title" placeholder="Title"
+              <input
+                name="title"
+                placeholder="Title"
                 value={formData.title}
                 onChange={handleChange}
-                required/>
+                className="w-full px-4 py-3 rounded-lg border border-slate-300 bg-slate-400 mb-3"
+                required
+              />
 
-                <textarea 
-                name= "description"
+              <textarea
+                name="description"
                 placeholder="Description"
                 value={formData.description}
                 onChange={handleChange}
-                required/>
-
-                {type === "assign" && (
-                    <select 
-                name="assignedTo"
-                value={formData.assignedTo}
-                onChange={handleChange}
+                className="w-full px-4 py-3 rounded-lg border border-slate-300 bg-slate-400 resize-none mb-3"
                 required
+              />
+
+              {type === "assign" && (
+                <select
+                  name="assignedTo"
+                  value={formData.assignedTo}
+                  onChange={handleChange}
+                  className="w-s px-4 py-3 rounded-lg border border-slate-300 bg-slate-400 mb-3"
+                  required
                 >
-                    <option value="">Select User</option>
-                   
-                    { users.map((user)=>(
-                        <option key={user._id} value={user._id}>
-                            {user.name}
-                        </option> 
-                    ))}
-                  
+                  <option value="">Select User</option>
+
+                  {users.map((user) => (
+                    <option key={user._id} value={user._id}>
+                      {user.name}
+                    </option>
+                  ))}
                 </select>
-                )}
-                
-                <button type="submit"> Create task</button>
-              
+              )}
+
+              <button
+                type="submit"
+                className="w-full bg-blue-600 text-white py-3 rounded-lg font-medium 
+            hover:bg-blue-700 transition"
+              >
+                {" "}
+                Create task
+              </button>
             </form>
             {message && <p>{message}</p>}
+          </div>
         </div>
-        </>
-        
+      </>
     );
 
 };
